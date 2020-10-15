c0342fe8e748bade6d1f2978f43759bf0f018248
DepressedCookie <vipc.1224@live.ca>
2020-10-15 18:15:48 -0400

    Init Project

---
+++ Assets/Scripts/Script_Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_Menu : MonoBehaviour
+{
+    [SerializeField]
+    string StartMapName;
+
+    public void OnStartButtonPressed()
+    {
+        SceneManager.LoadScene(StartMapName);
+    }
+}
