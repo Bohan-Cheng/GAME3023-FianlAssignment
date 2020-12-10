337e26367cdff1a4e175eb7b4b095bc79cf2c54f
DepressedCookie <vipc.1224@live.ca>
2020-12-09 23:44:09 -0500

    Implemented player movement animation

--- Assets/Characters/Scripts/PlayerCharacterController.cs
+++ Assets/Characters/Scripts/PlayerCharacterController.cs
@@ -10,13 +10,34 @@ public class PlayerCharacterController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidBody;
 
+    private Animator anim;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        float MoveX = Input.GetAxis("Horizontal");
+        float MoveY = Input.GetAxis("Vertical");
+        Vector2 movementVector = new Vector2(MoveX, MoveY);
+
+        if(MoveX > 0.1f) { anim.SetInteger("Direction", 3); }
+        if(MoveX < -0.1f) { anim.SetInteger("Direction", 2); }
+        if(MoveY > 0.1f) { anim.SetInteger("Direction", 0); }
+        if(MoveY < -0.1f) { anim.SetInteger("Direction", 1); }
+
         if(rigidBody.velocity.magnitude > 0.5f)
         {
             GetComponent<Script_RandomBattle>().AddStep();
+            anim.speed = 1.0f;
+        }
+        else if(rigidBody.velocity.magnitude <= 0.1f)
+        {
+            anim.speed = 0.0f;
         }
         movementVector *= speed;
         rigidBody.velocity = movementVector;
